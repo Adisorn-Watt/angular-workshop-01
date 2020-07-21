342336cbb09d2c48b687f826501c4cf83f9bf0bb
Adisorn Wattana <adisorn.wattana@allianz.com>
2020-07-21 11:44:22 +0700

    [Update] Apply Reactive Forms Module and Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UsersComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, UsersComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 type UserState = {
-  name: string;
+  fullName: string;
   strikeThrough: boolean;
 };
 
@@ -11,17 +12,28 @@ type UserState = {
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  firstName = '';
-  lastName = '';
-  fullName = '';
+  //firstName = '';
+  //lastName = '';
+  //fullName = '';
   users: Array<UserState> = []; //Array
-  errCheck:boolean = false;
+  //errfName:boolean = false;
+  //errlName:boolean = false;
   countStrikeThrough: number = 0;
-  state: boolean = false;
+  //state: boolean = false;
+  summitted = false;
 
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
   }
 
   checkStrikeThrough (user: UserState): void {
@@ -34,24 +46,32 @@ export class UsersComponent implements OnInit {
     }
   }
 
-  inputFirstName(event: any): void {
+  /*inputFirstName(event: any): void {
     this.firstName = event.target.value;
   }
 
   inputLastname(input: string): void {
     this.lastName = input;
-  }
+  }*/
 
   saveData(): void {
     // There's only empty, or is/are only space(s)
-    if (this.firstName === "" || this.firstName == null) {
+    /*if (this.firstName === "" || this.firstName == null) {
       this.errCheck = true;
       return;
-    } 
+    } */
     // Normal case
-    this.errCheck = false
-    this.fullName = `${this.firstName} ${this.lastName}`;
+    //this.errCheck = false
+    //this.fullName = `${this.firstName} ${this.lastName}`;
     // Append to list/array
-    this.users.push({name: this.fullName, strikeThrough: false});
+    //this.users.push({name: this.fullName, strikeThrough: false});
+    this.summitted = true;
+    if (this.loginForm.invalid) {
+
+      return;
+    }
+
+    const fullName = `${this.f.firstName.value} ${this.f.lastName.value}`;
+    this.users.push({fullName: fullName, strikeThrough: false});
   }
 }
